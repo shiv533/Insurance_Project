65d9713f1893c5353273ea72c3299157efb16cc4
Shivam <shivammpanchal2002@gmail.com>
2025-02-21 21:51:50 +0530

    This is Industrial project of Group D message added

---
+++ Insurance_Management_System/src/main/java/com/example/demo/restcontroller/InsuranceController.java
@@ -0,0 +1,14 @@
+package com.example.demo.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/api")
+public class InsuranceController {
+
+	@GetMapping("/hello")
+	public String helloTest() {
+		return "This is Industrial project of Group D";
+	}
+}
