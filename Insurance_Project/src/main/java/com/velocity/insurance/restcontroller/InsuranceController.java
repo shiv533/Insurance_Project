8a58c84424dc40203d5aacd784693c31d2b7ad1b
Shivam <shivammpanchal2002@gmail.com>
2025-02-22 13:29:29 +0530

    Changed the package structure

--- Insurance_Project/src/main/java/com/velocity/insurance/restcontroller/InsuranceController.java
+++ Insurance_Project/src/main/java/com/velocity/insurance/restcontroller/InsuranceController.java
@@ -1,4 +1,4 @@
-package com.example.demo.restcontroller;
+package com.velocity.insurance.restcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Insurance_Project/src/test/java/com/velocity/insurance/InsuranceProjectApplicationTests.java
+++ Insurance_Project/src/test/java/com/velocity/insurance/InsuranceProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.velocity.insurance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InsuranceManagementSystemApplicationTests {
+class InsuranceProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
